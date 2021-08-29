2de4a1e634c026f94f33ef230d6a04204ea7cbd5
Lorenzo Costa <costa86@zoho.com>
2021-08-29 21:07:31 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,7 +1,16 @@
 def buildApp() {
-    echo 'building'
+    sh """
+    touch ola.txt
+    echo Build for ${params.CUSTOMER}
+    scp -i ola.txt $SSH_KEY:
+    """
 }
+
 def testApp() {
+    echo 'testing'
+}
+
+def deployApp() {
     echo 'testing'
 }
 
