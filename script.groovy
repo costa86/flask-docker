07e8b1479c4ad83431267622983ad981795880a8
Lorenzo Costa <costa86@zoho.com>
2021-08-29 21:48:51 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,10 +1,5 @@
 def buildApp() {
-    sh """
-    touch ola.txt
-    echo Build for ${params.CUSTOMER}
-    ls -la
-    'scp -i $SSH_KEY hello.txt costa@142.93.100.139:'
-    """
+    sh 'scp -i $SSH_KEY hello.txt costa@142.93.100.139:'
 }
 
 def testApp() {
